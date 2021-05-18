d152ffb37406efc09c6732fc79977008fa3c450a
Alessandro <Alessandro@LAPTOP-2Q5TD8MO.station>
2021-05-18 11:15:55 +0200

    cambio password

--- src/main/java/it/polito/tdp/rivers/db/DBConnect.java
+++ src/main/java/it/polito/tdp/rivers/db/DBConnect.java
@@ -20,7 +20,7 @@ public class DBConnect {
 			HikariConfig config = new HikariConfig();
 			config.setJdbcUrl(jdbcURL);
 			config.setUsername("root");
-			config.setPassword("");
+			config.setPassword("10roma10");
 			
 			// configurazione MySQL
 			config.addDataSourceProperty("cachePrepStmts", "true");
